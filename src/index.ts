6ddca7c7abd5a9f65db554c4aec5844464bfe34d
Aurélien Noce <aurelien@noce.fr>
2019-01-04 16:28:40 +0100

    Export dynamicBind

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 // Re-export all the core function
 export { default as bind } from "./bind"
 export { default as bindStream } from "./bindStream"
+export { default as dynamicBind } from "./dynamicBind"
 // and also the utilities
 export { componentFromStream, createEventHandler } from "./utils"
 // and relevant types
